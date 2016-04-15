e52128a6b7810c8eb2947c13c31b98a0a2ca4680
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-15 11:32:11 -0300

    new test

---
+++ tests/CurrencyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Mateusjatenee\Currency\Currency;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class CurrencyTest extends PHPUnit
+{
+    public function testAllMethodReturnsAnArray()
+    {
+        $currency = new Currency(10);
+
+        $currency = $currency->all('USD');
+
+        $this->assertArrayHasKey('BRL');
+        $this->assertArrayNotHasKey('USD');
+    }
+}
